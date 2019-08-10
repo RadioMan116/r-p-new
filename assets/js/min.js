63fa4fb68cf47d2ec1db1dafcaea646a05a156aa
RadioMan116 <noris75564@gmail.com>
2019-08-10 15:11:40 +0300

    master

--- assets/js/min.js
+++ assets/js/min.js
@@ -10721,6 +10721,8 @@ $(document).ready(function () {
 	var swiper = new Swiper('.right .swiper-container', {
 		slidesPerView: 1,
 		spaceBetween: 0,
+		effect: 'slide',
+		// direction: 'vertical',
 		pagination: {
 			el: '.right .swiper-pagination'
 		},
@@ -10741,6 +10743,47 @@ $(document).ready(function () {
 			nextEl: '.work-examples .swiper-button-next',
 			prevEl: '.work-examples .swiper-button-prev',
 		},
+	});
+	var galleryThumbs = new Swiper('.gallery-thumbs', {
+		spaceBetween: 10,
+		slidesPerView: 6,
+		allowTouchMove: false,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+	});
+	var galleryTop = new Swiper('.gallery-top', {
+		spaceBetween: 10,
+		allowTouchMove: false,
+		// navigation: {
+		// 	nextEl: '.gallery-top .swiper-button-next',
+		// 	prevEl: '.gallery-top .swiper-button-prev',
+		// },
+		thumbs: {
+			swiper: galleryThumbs
+		}
+	});
+	var swiper = new Swiper('.services__children-container', {
+		effect: 'cube',
+		grabCursor: true,
+		cubeEffect: {
+			shadow: true,
+			slideShadows: true,
+			shadowOffset: 20,
+			shadowScale: 0.94,
+		},
+		navigation: {
+
+			nextEl: '.services .swiper-button-next',
+			prevEl: '.services .swiper-button-prev',
+		},
+		// autoplay: {
+		// 	delay: 3500,
+		// 	disableOnInteraction: true,
+		// },
+		// pagination: {
+		// 	el: '.swiper-pagination',
+		// },
 	});
 
 	$('#myForm form').on('submit', function () {
@@ -10766,6 +10809,40 @@ $(document).ready(function () {
 		}, 'json');
 
 		return false;
+	});
+
+	var token = '2025232371.1677ed0.ade052bc379842c09ee22809a5e84d21',
+		num_photos = 10;
+
+	$.ajax({
+		url: 'https://api.instagram.com/v1/users/self/media/recent',
+		dataType: 'jsonp',
+		type: 'GET',
+		data: {
+			access_token: token,
+			count: num_photos
+		},
+		success: function (data) {
+			console.log(data);
+			for (x in data.data) {
+				$('#rudr_instafeed .swiper-wrapper').append('<div class="swiper-slide"><a data-fancybox="photo" href="' + data.data[x].images.standard_resolution.url + '"><img src="' + data.data[x].images.thumbnail.url + '"></a></div>');
+				var swiper5 = new Swiper('#rudr_instafeed.swiper-container', {
+					slidesPerView: 5,
+					spaceBetween: 0,
+					pagination: {
+						el: '#rudr_instafeed .swiper-pagination'
+					},
+					navigation: {
+
+						nextEl: '#rudr_instafeed .swiper-button-next',
+						prevEl: '#rudr_instafeed .swiper-button-prev',
+					},
+				});
+			}
+		},
+		error: function (data) {
+			console.log(data);
+		}
 	});
 
 });
